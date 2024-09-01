73f2ca999591a58bf89f80167804e947081b452a
JillianKoszuta <jilliankoszuta@gmail.com>
2024-08-31 23:53:19 -0400

    added note file

---
+++ FreeCodeCampNotes/Main.js
@@ -0,0 +1,8 @@
+// start with let, const, or var to create a variable
+let character = "Hello"; //I create a variable(character) with the value (hello)
+console.log(character); // display what the value(hello) is for the variable (character)
+character = "World"; //forces character to change from hello to world
+
+let secondCharacter; //I create a variable(secondCharacter) - there is no value
+secondCharacter = "Test"; //I have a variable(secondCharacter) without a value, I set the value to be test
+console.log(secondCharacter); //display the value(test) of secondCharacter(variable)
---
+++ FreeCodeCampNotes/scratch.js
@@ -0,0 +1,5 @@
+let first = "One"; // first is equal to one
+let second = "Two"; // second is equal to two
+console.log(second); // tell me what second is equal to. It displays, two
+second = first; // set the value of the variable to first (another variable. its value is one). The value of second becomes One
+console.log(second); // tell me what second is equal to. It displays, One
